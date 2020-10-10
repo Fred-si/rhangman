513d09de7694aae83cd669187d63deb9d4d5893b
fred-si <fred-si@zaclys.net>
2020-10-10 14:38:03 +0200

    Met en place le squelette de la page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1>Le jeu du pendu</h1>
+      <div className="App">
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
